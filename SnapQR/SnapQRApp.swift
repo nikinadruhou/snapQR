5f38a472a016497e8e2c299f6a06b82f32a28a9d
jura8686 <jura.krystof@seznam.cz>
2025-06-26 20:44:45 +0200

    Initial Commit

---
+++ SnapQR/SnapQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapQRApp.swift
+//  SnapQR
+//
+//  Created by Jiří Jedlička on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnapQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapQRTests/SnapQRTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapQRTests.swift
+//  SnapQRTests
+//
+//  Created by Jiří Jedlička on 26.06.2025.
+//
+
+import Testing
+@testable import SnapQR
+
+struct SnapQRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
